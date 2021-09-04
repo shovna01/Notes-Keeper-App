e7fce0eaa5c75d3ac5bc12c2e19e482250305007
Shovna <shovnapanda01@gmail.com>
2021-09-04 22:41:55 +0530

    Initial Commit

---
+++ build/precache-manifest.6d6bee17e9cc21f49d7e0989952ff0f0.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6dcc3a725a15f87c98bdb7e11a573e8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "47c9633f38d0a86aa881",
+    "url": "/static/js/2.983cee07.chunk.js"
+  },
+  {
+    "revision": "d65d36889c6f518c55ea",
+    "url": "/static/js/main.de1e1ea3.chunk.js"
+  },
+  {
+    "revision": "583e51a7281672351041",
+    "url": "/static/js/runtime-main.38f647cd.js"
+  }
+]);
